c0dd570eee2bad2ab80206e6aaa5e8930198cef9
lucasvmiguel <lucasvmiguel@live.com>
2016-05-04 00:09:33 -0300

    docker correto

--- main.sh
+++ main.sh
@@ -1,35 +1,36 @@
 #install git
 sudo apt-get update
-sudo apt-get install git
+sudo apt-get install git -y
 
 #install atom
 sudo add-apt-repository ppa:webupd8team/atom
 sudo apt-get update
-sudo apt-get install atom
-
-#install docker
-sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-get install atom -y
 
 #install zsh
 sudo apt-get update
-sudo apt-get install zsh
+sudo apt-get install zsh -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 #install chrome
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+wget -qO- https://get.docker.com/ | shwget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
-sudo apt-get install google-chrome-stable
+sudo apt-get install google-chrome-stable -y
 
 #install erlang && elixir && phoenix
 wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
 sudo apt-get update
-sudo apt-get install esl-erlang
-sudo apt-get install elixir
+sudo apt-get install esl-erlang -y
+sudo apt-get install elixir -y
 mix archive.install https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez
 
 #install nodejs
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
+
+#install vim
+sudo apt-get install vim -y
+
+#install docker
+wget -qO- https://get.docker.com/ | sh
