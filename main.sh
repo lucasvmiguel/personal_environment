6018994253e7cc2029e2b38bd272dc6bd1271a87
lucas <lucasvmiguel@live.com>
2016-05-07 15:15:12 -0300

    acertos

--- main.sh
+++ main.sh
@@ -7,23 +7,12 @@ sudo add-apt-repository ppa:webupd8team/atom
 sudo apt-get update
 sudo apt-get install atom -y
 
-#install zsh
-sudo apt-get update
-sudo apt-get install zsh -y
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-#install chrome
-wget -qO- https://get.docker.com/ | shwget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt-get update
-sudo apt-get install google-chrome-stable -y
-
 #install erlang && elixir && phoenix
 wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
 sudo apt-get update
 sudo apt-get install esl-erlang -y
 sudo apt-get install elixir -y
-mix archive.install https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez
+mix archive.install https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez -y
 
 #install nodejs
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
@@ -40,3 +29,13 @@ sudo apt-get install atom -y
 
 #install client postgres
 sudo apt-get install postgresql postgresql-contrib -y
+
+#install chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get install google-chrome-stable -y
+
+#install zsh
+sudo apt-get update
+sudo apt-get install zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
